36f000ae97685b705027b4d9e05a0840e3f15dd9
tomo <tomo.mouse.9086@gmail.com>
2019-03-12 21:55:34 +0900

    inital commit

---
+++ main.js
@@ -0,0 +1,63 @@
+const cvs = document.querySelector("#canvas")
+const ctx = cvs.getContext("2d")
+const fileobj = document.querySelector("#file")
+const colorobj = document.querySelector("#color-picker")
+
+const img = new Image()
+let exifstr = ""
+let filename = ""
+
+img.onload = () => {
+	EXIF.getData(img, function() {
+		const data = EXIF.getAllTags(this)
+		console.log(data)
+		cvs.setAttribute("width", data.PixelXDimension)
+		cvs.setAttribute("height", data.PixelYDimension)
+		exifstr = `${data.Model} ${data.FocalLength}mm F${data.FNumber} ${data.ExposureTime.numerator}/${data.ExposureTime.denominator} ISO ${data.ISOSpeedRatings} Photo by ${data.Artist}`
+	})
+	ctx.drawImage(img, 0, 0)
+}
+
+fileobj.addEventListener("change", e => {
+	const file = e.target.files[0]
+	const reader = new FileReader()
+	reader.readAsDataURL(file)
+	reader.onload = () => {
+		img.src = reader.result
+		filename = file.name
+	}
+}, false)
+colorobj.addEventListener("change", e => {
+	drawText()
+}, false)
+
+function drawText() {
+	ctx.fillStyle = colorobj.value
+	ctx.font = "80px meiryo"
+	ctx.fillText(exifstr, 100, 150)
+}
+function download() {
+	// const link = document.createElement("a")
+	const linkcontainer = document.querySelector("#download-container")
+	// for(let node of linkcontainer.childNodes) {
+	// 	console.log(node)
+	// 	linkcontainer.removeChild(node)
+	// }
+	while(linkcontainer.hasChildNodes()) {
+		linkcontainer.removeChild(linkcontainer.lastChild)
+	}
+	const link = document.createElement("a")
+	link.innerText = "Click here"
+	linkcontainer.appendChild(link)
+	const span = document.createElement("span")
+	span.innerText = " if the download doesn't start automatically."
+	linkcontainer.appendChild(span)
+	// link.href = cvs.toDataURL("image/jpeg")
+	// link.download = "canvas.png"
+	// link.click()
+	cvs.toBlob(bin => {
+		link.href = URL.createObjectURL(bin)
+		link.download = filename || "canvas.jpg"
+		link.click()
+	}, "image/jpeg")
+}
